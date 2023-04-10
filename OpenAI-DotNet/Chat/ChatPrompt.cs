877ec2fb747a17042d2271ae0fe926ebd6f6727e
Stephen Hodgson <hodgson.designs@gmail.com>
2023-04-10 00:49:55 -0400

    OpenAI-DotNet 6.7.0
    - Deprecated ChatPrompt -> Message
    - Added Role enum for chat messages
    - Update ChatRequest constructor to use IEnumerable<Message> messages
    - Update ChatRequest.Messages to IReadonlyList<Message>
    - Updated unit tests

--- OpenAI-DotNet/Chat/ChatPrompt.cs
+++ OpenAI-DotNet/Chat/ChatPrompt.cs
@@ -1,23 +1,40 @@
-using System.Text.Json;
+using System;
 using System.Text.Json.Serialization;
 
 namespace OpenAI.Chat
 {
+    [Obsolete("Use OpenAI.Chat.Message instead")]
     public sealed class ChatPrompt
     {
-        [JsonConstructor]
+        [Obsolete("Use OpenAI.Chat.Message instead")]
         public ChatPrompt(string role, string content)
+        {
+            Role = role.ToLower() switch
+            {
+                "system" => Role.System,
+                "assistant" => Role.Assistant,
+                "user" => Role.User,
+                _ => throw new ArgumentException(nameof(role))
+            };
+            Content = content;
+        }
+
+        [JsonConstructor]
+        [Obsolete("Use OpenAI.Chat.Message instead")]
+        public ChatPrompt(Role role, string content)
         {
             Role = role;
             Content = content;
         }
 
         [JsonPropertyName("role")]
-        public string Role { get; }
+        public Role Role { get; }
 
         [JsonPropertyName("content")]
         public string Content { get; }
 
-        public override string ToString() => JsonSerializer.Serialize(this);
+        public static implicit operator ChatPrompt(Message message) => new ChatPrompt(message.Role, message.Content);
+
+        public static implicit operator Message(ChatPrompt message) => new Message(message.Role, message.Content);
     }
 }
--- OpenAI-DotNet/Chat/ChatResponse.cs
+++ OpenAI-DotNet/Chat/ChatResponse.cs
@@ -33,7 +33,5 @@ namespace OpenAI.Chat
 
         [JsonIgnore]
         public Choice FirstChoice => Choices.FirstOrDefault();
-
-        public override string ToString() => JsonSerializer.Serialize(this);
     }
 }
