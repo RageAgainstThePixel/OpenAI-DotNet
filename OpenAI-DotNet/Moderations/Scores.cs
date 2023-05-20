3bc838dcc9c2411d4075126bbd5b710e287be464
damiant3 <131276826+damiant3@users.noreply.github.com>
2023-05-20 07:45:56 -0700

    Update Scores.cs

--- OpenAI-DotNet/Moderations/Scores.cs
+++ OpenAI-DotNet/Moderations/Scores.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Text;
+using System.Text.Json.Serialization;
 
 namespace OpenAI.Moderations
 {
@@ -31,5 +32,20 @@ namespace OpenAI.Moderations
         [JsonInclude]
         [JsonPropertyName("violence/graphic")]
         public double ViolenceGraphic { get; private set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder()
+                .AppendLine($"{"Hate:".PadRight(10)}{Hate:0.00 E+00}")
+                .AppendLine($"{"Threat:".PadRight(10)}{HateThreatening:0.00 E+00}")
+                .AppendLine($"{"Violence:".PadRight(10)}{Violence:0.00 E+00}")
+                .AppendLine($"{"Graphic:".PadRight(10)}{ViolenceGraphic:0.00 E+00}")
+                .AppendLine($"{"SelfHarm:".PadRight(10)}{SelfHarm:0.00 E+00}")
+                .AppendLine($"{"Sexual:".PadRight(10)}{Sexual:0.00 E+00}")
+                .AppendLine($"{"Minors:".PadRight(10)}{SexualMinors:0.00 E+00}");
+            return sb.ToString();
+        }
+
+        public static implicit operator string(Scores scores) => scores.ToString();
     }
 }
