cd94c2ab278592a147b71ff2532374cb8e444671
damiant3 <131276826+damiant3@users.noreply.github.com>
2023-05-20 08:17:19 -0700

    Update Scores.cs

--- OpenAI-DotNet/Moderations/Scores.cs
+++ OpenAI-DotNet/Moderations/Scores.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Text.Json.Serialization;
 
 namespace OpenAI.Moderations
@@ -35,15 +36,13 @@ namespace OpenAI.Moderations
 
         public override string ToString()
         {
-            var sb = new StringBuilder()
-                .AppendLine($"{"Hate:".PadRight(10)}{Hate:0.00 E+00}")
-                .AppendLine($"{"Threat:".PadRight(10)}{HateThreatening:0.00 E+00}")
-                .AppendLine($"{"Violence:".PadRight(10)}{Violence:0.00 E+00}")
-                .AppendLine($"{"Graphic:".PadRight(10)}{ViolenceGraphic:0.00 E+00}")
-                .AppendLine($"{"SelfHarm:".PadRight(10)}{SelfHarm:0.00 E+00}")
-                .AppendLine($"{"Sexual:".PadRight(10)}{Sexual:0.00 E+00}")
-                .AppendLine($"{"Minors:".PadRight(10)}{SexualMinors:0.00 E+00}");
-            return sb.ToString();
+            return $"{"Hate:",-10}{Hate:0.00 E+00}" + Environment.NewLine
+                + $"{"Threat:",-10}{HateThreatening:0.00 E+00}" + Environment.NewLine
+                + $"{"Violence:",-10}{Violence:0.00 E+00}" + Environment.NewLine
+                + $"{"Graphic:",-10}{ViolenceGraphic:0.00 E+00}" + Environment.NewLine
+                + $"{"SelfHarm:",-10}{SelfHarm:0.00 E+00}" + Environment.NewLine
+                + $"{"Sexual:",-10}{Sexual:0.00 E+00}" + Environment.NewLine
+                + $"{"Minors:",-10}{SexualMinors:0.00 E+00}" + Environment.NewLine;
         }
 
         public static implicit operator string(Scores scores) => scores.ToString();
