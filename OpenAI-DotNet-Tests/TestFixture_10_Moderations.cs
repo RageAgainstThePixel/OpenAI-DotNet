719de0772ddb1d5426437f716db839698833f2c4
Stephen Hodgson <hodgson.designs@gmail.com>
2023-11-27 12:51:33 -0500

    bump version
    update tests

--- OpenAI-DotNet-Tests/TestFixture_10_Moderations.cs
+++ OpenAI-DotNet-Tests/TestFixture_10_Moderations.cs
@@ -1,38 +1,35 @@
-﻿using System;
-using NUnit.Framework;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 using OpenAI.Moderations;
+using System;
+using System.Threading.Tasks;
 
 namespace OpenAI.Tests
 {
     internal class TestFixture_10_Moderations : AbstractTestFixture
     {
         [Test]
-        public async Task Test_1_Moderate()
+        public async Task Test_01_Moderate()
         {
             Assert.IsNotNull(OpenAIClient.ModerationsEndpoint);
-
             var isViolation = await OpenAIClient.ModerationsEndpoint.GetModerationAsync("I want to kill them.");
             Assert.IsTrue(isViolation);
+        }
 
-            var isChunkedViolation = await OpenAIClient.ModerationsEndpoint.GetModerationChunkedAsync("I want to kill them.");
-            Assert.IsTrue(isChunkedViolation);
-            
+        [Test]
+        public async Task Test_02_Moderate_Scores()
+        {
+
             var response = await OpenAIClient.ModerationsEndpoint.CreateModerationAsync(new ModerationsRequest("I love you"));
             Assert.IsNotNull(response);
             Console.WriteLine(response.Results?[0]?.Scores?.ToString());
         }
 
         [Test]
-        public async Task Test_2_ModerationChunked()
+        public async Task Test_03_Moderation_Chunked()
         {
             Assert.IsNotNull(OpenAIClient.ModerationsEndpoint);
-
-            var isChunkedViolation = await OpenAIClient.ModerationsEndpoint.GetModerationChunkedAsync(
-                "I don't want to kill them. I want to kill them. I want to kill them.", chunkSize: "I don't want to kill them.".Length,
-                chunkOverlap: 4);
-
-            Assert.IsTrue(isChunkedViolation);
+            var isViolation = await OpenAIClient.ModerationsEndpoint.GetModerationChunkedAsync("I don't want to kill them. I want to kill them. I want to kill them.", chunkSize: "I don't want to kill them.".Length, chunkOverlap: 4);
+            Assert.IsTrue(isViolation);
         }
     }
 }
