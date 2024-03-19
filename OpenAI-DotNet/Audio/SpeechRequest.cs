e72c0a701ca78bf49e13a568d8ac3b8d5b8f8d6a
Stephen Hodgson <hodgson.designs@gmail.com>
2024-03-18 21:33:48 -0400

    Updated SpeechResponseFormat to include wav and pcm

--- OpenAI-DotNet/Audio/SpeechRequest.cs
+++ OpenAI-DotNet/Audio/SpeechRequest.cs
@@ -14,7 +14,7 @@ namespace OpenAI.Audio
         /// <param name="input">The text to generate audio for. The maximum length is 4096 characters.</param>
         /// <param name="model">One of the available TTS models. Defaults to tts-1.</param>
         /// <param name="voice">The voice to use when generating the audio.</param>
-        /// <param name="responseFormat">The format to audio in. Supported formats are mp3, opus, aac, and flac.</param>
+        /// <param name="responseFormat">The format to audio in. Supported formats are mp3, opus, aac, flac, wav and pcm.</param>
         /// <param name="speed">The speed of the generated audio. Select a value from 0.25 to 4.0. 1.0 is the default.</param>
         public SpeechRequest(string input, Model model = null, SpeechVoice voice = SpeechVoice.Alloy, SpeechResponseFormat responseFormat = SpeechResponseFormat.MP3, float? speed = null)
         {
@@ -44,7 +44,7 @@ namespace OpenAI.Audio
         public SpeechVoice Voice { get; }
 
         /// <summary>
-        /// The format to audio in. Supported formats are mp3, opus, aac, and flac.
+        /// The format to audio in. Supported formats are mp3, opus, aac, flac, wav and pcm.
         /// </summary>
         [JsonPropertyName("response_format")]
         [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
--- OpenAI-DotNet/Extensions/ResponseExtensions.cs
+++ OpenAI-DotNet/Extensions/ResponseExtensions.cs
@@ -7,7 +7,6 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Net.Http.Json;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
