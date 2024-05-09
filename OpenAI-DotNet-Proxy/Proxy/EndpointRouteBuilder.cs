a2b208897d56238271b0c586d35dc0c3182b7c23
Stephen Hodgson <hodgson.designs@gmail.com>
2024-05-08 21:56:40 -0400

    OpenAI-DotNet-Proxy 7.7.9
    - Slight refactor of OpenAIProxyStartup to remove duplicate code
    - Added OpenAI.Proxy.EndpointRouteBuilder

---
+++ OpenAI-DotNet-Proxy/Proxy/EndpointRouteBuilder.cs
@@ -0,0 +1,128 @@
+﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Authentication;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace OpenAI.Proxy
+{
+    public static class EndpointRouteBuilder
+    {
+        // Copied from https://github.com/microsoft/reverse-proxy/blob/51d797986b1fea03500a1ad173d13a1176fb5552/src/ReverseProxy/Forwarder/RequestUtilities.cs#L61-L83
+        private static readonly HashSet<string> excludedHeaders = new()
+        {
+            HeaderNames.Connection,
+            HeaderNames.TransferEncoding,
+            HeaderNames.KeepAlive,
+            HeaderNames.Upgrade,
+            "Proxy-Connection",
+            "Proxy-Authenticate",
+            "Proxy-Authentication-Info",
+            "Proxy-Authorization",
+            "Proxy-Features",
+            "Proxy-Instruction",
+            "Security-Scheme",
+            "ALPN",
+            "Close",
+            "Set-Cookie",
+            HeaderNames.TE,
+#if NET
+            HeaderNames.AltSvc,
+#else
+            "Alt-Svc",
+#endif
+        };
+
+        public static void MapOpenAIEndpoints(this IEndpointRouteBuilder endpoints, OpenAIClient openAIClient, IAuthenticationFilter authenticationFilter)
+        {
+            endpoints.Map($"{openAIClient.OpenAIClientSettings.BaseRequest}{{**endpoint}}", HandleRequest);
+
+            async Task HandleRequest(HttpContext httpContext, string endpoint)
+            {
+                try
+                {
+                    // ReSharper disable once MethodHasAsyncOverload
+                    // just in case either method is implemented we call it twice.
+                    authenticationFilter.ValidateAuthentication(httpContext.Request.Headers);
+                    await authenticationFilter.ValidateAuthenticationAsync(httpContext.Request.Headers);
+
+                    var method = new HttpMethod(httpContext.Request.Method);
+                    var uri = new Uri(string.Format(
+                            openAIClient.OpenAIClientSettings.BaseRequestUrlFormat,
+                            $"{endpoint}{httpContext.Request.QueryString}"
+                        ));
+                    using var request = new HttpRequestMessage(method, uri);
+                    request.Content = new StreamContent(httpContext.Request.Body);
+
+                    if (httpContext.Request.ContentType != null)
+                    {
+                        request.Content.Headers.ContentType = System.Net.Http.Headers.MediaTypeHeaderValue.Parse(httpContext.Request.ContentType);
+                    }
+
+                    var proxyResponse = await openAIClient.Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+                    httpContext.Response.StatusCode = (int)proxyResponse.StatusCode;
+
+                    foreach (var (key, value) in proxyResponse.Headers)
+                    {
+                        if (excludedHeaders.Contains(key))
+                        {
+                            continue;
+                        }
+
+                        httpContext.Response.Headers[key] = value.ToArray();
+                    }
+
+                    foreach (var (key, value) in proxyResponse.Content.Headers)
+                    {
+                        if (excludedHeaders.Contains(key))
+                        {
+                            continue;
+                        }
+
+                        httpContext.Response.Headers[key] = value.ToArray();
+                    }
+
+                    httpContext.Response.ContentType = proxyResponse.Content.Headers.ContentType?.ToString() ?? string.Empty;
+                    const string streamingContent = "text/event-stream";
+
+                    if (httpContext.Response.ContentType.Equals(streamingContent))
+                    {
+                        var stream = await proxyResponse.Content.ReadAsStreamAsync();
+                        await WriteServerStreamEventsAsync(httpContext, stream);
+                    }
+                    else
+                    {
+                        await proxyResponse.Content.CopyToAsync(httpContext.Response.Body);
+                    }
+                }
+                catch (AuthenticationException authenticationException)
+                {
+                    httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await httpContext.Response.WriteAsync(authenticationException.Message);
+                }
+                catch (Exception e)
+                {
+                    httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    var response = JsonSerializer.Serialize(new { error = new { e.Message, e.StackTrace } });
+                    await httpContext.Response.WriteAsync(response);
+                }
+
+                static async Task WriteServerStreamEventsAsync(HttpContext httpContext, Stream contentStream)
+                {
+                    var responseStream = httpContext.Response.Body;
+                    await contentStream.CopyToAsync(responseStream, httpContext.RequestAborted);
+                    await responseStream.FlushAsync(httpContext.RequestAborted);
+                }
+            }
+        }
+    }
+}
--- OpenAI-DotNet-Proxy/Proxy/OpenAIProxyStartup.cs
+++ OpenAI-DotNet-Proxy/Proxy/OpenAIProxyStartup.cs
@@ -3,18 +3,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Net.Http.Headers;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Security.Authentication;
-using System.Text.Json;
 using System.Threading.Tasks;
-using MediaTypeHeaderValue = System.Net.Http.Headers.MediaTypeHeaderValue;
 
 namespace OpenAI.Proxy
 {
@@ -26,33 +19,18 @@ namespace OpenAI.Proxy
         private OpenAIClient openAIClient;
         private IAuthenticationFilter authenticationFilter;
 
-        // Copied from https://github.com/microsoft/reverse-proxy/blob/51d797986b1fea03500a1ad173d13a1176fb5552/src/ReverseProxy/Forwarder/RequestUtilities.cs#L61-L83
-        private static readonly HashSet<string> ExcludedHeaders = new()
-        {
-            HeaderNames.Connection,
-            HeaderNames.TransferEncoding,
-            HeaderNames.KeepAlive,
-            HeaderNames.Upgrade,
-            "Proxy-Connection",
-            "Proxy-Authenticate",
-            "Proxy-Authentication-Info",
-            "Proxy-Authorization",
-            "Proxy-Features",
-            "Proxy-Instruction",
-            "Security-Scheme",
-            "ALPN",
-            "Close",
-            "Set-Cookie",
-            HeaderNames.TE,
-#if NET
-            HeaderNames.AltSvc,
-#else
-            "Alt-Svc",
-#endif
-        };
+        /// <summary>
+        /// Configures the <see cref="OpenAIClient"/> and <see cref="IAuthenticationFilter"/> services.
+        /// </summary>
+        /// <param name="services">Services collection.</param>
+        public void ConfigureServices(IServiceCollection services)
+            => SetupServices(services.BuildServiceProvider());
 
-        public void ConfigureServices(IServiceCollection services) { }
-
+        /// <summary>
+        /// Configures the <see cref="IApplicationBuilder"/> to handle requests and forward them to OpenAI API.
+        /// </summary>
+        /// <param name="app"><see cref="IApplicationBuilder"/>.</param>
+        /// <param name="env"><see cref="IWebHostEnvironment"/>.</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -60,15 +38,14 @@ namespace OpenAI.Proxy
                 app.UseDeveloperExceptionPage();
             }
 
-            openAIClient = app.ApplicationServices.GetRequiredService<OpenAIClient>();
-            authenticationFilter = app.ApplicationServices.GetRequiredService<IAuthenticationFilter>();
+            SetupServices(app.ApplicationServices);
 
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/health", HealthEndpoint);
-                endpoints.Map($"{openAIClient.OpenAIClientSettings.BaseRequest}{{**endpoint}}", HandleRequest);
+                endpoints.MapOpenAIEndpoints(openAIClient, authenticationFilter);
             });
         }
 
@@ -79,26 +56,17 @@ namespace OpenAI.Proxy
         /// <param name="args">Startup args.</param>
         /// <param name="openAIClient"><see cref="OpenAIClient"/> with configured <see cref="OpenAIAuthentication"/> and <see cref="OpenAIClientSettings"/>.</param>
         public static IHost CreateDefaultHost<T>(string[] args, OpenAIClient openAIClient) where T : class, IAuthenticationFilter
-        {
-            return Host.CreateDefaultBuilder(args)
+            => Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<OpenAIProxyStartup>();
-                    webBuilder.ConfigureKestrel(options =>
-                    {
-                        options.AllowSynchronousIO = false;
-                        options.Limits.MinRequestBodyDataRate = null;
-                        options.Limits.MinResponseDataRate = null;
-                        options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(10);
-                        options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(2);
-                    });
+                    webBuilder.ConfigureKestrel(ConfigureKestrel);
                 })
                 .ConfigureServices(services =>
                 {
                     services.AddSingleton(openAIClient);
                     services.AddSingleton<IAuthenticationFilter, T>();
                 }).Build();
-        }
 
         /// <summary>
         /// Creates a new <see cref="WebApplication"/> that acts as a proxy web api for OpenAI.
@@ -109,20 +77,28 @@ namespace OpenAI.Proxy
         public static WebApplication CreateWebApplication<T>(string[] args, OpenAIClient openAIClient) where T : class, IAuthenticationFilter
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.WebHost.ConfigureKestrel(options =>
-            {
-                options.AllowSynchronousIO = false;
-                options.Limits.MinRequestBodyDataRate = null;
-                options.Limits.MinResponseDataRate = null;
-                options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(10);
-                options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(2);
-            });
+            builder.WebHost.ConfigureKestrel(ConfigureKestrel);
             builder.Services.AddSingleton(openAIClient);
             builder.Services.AddSingleton<IAuthenticationFilter, T>();
             var app = builder.Build();
             var startup = new OpenAIProxyStartup();
             startup.Configure(app, app.Environment);
             return app;
+        }
+
+        private static void ConfigureKestrel(KestrelServerOptions options)
+        {
+            options.AllowSynchronousIO = false;
+            options.Limits.MinRequestBodyDataRate = null;
+            options.Limits.MinResponseDataRate = null;
+            options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(10);
+            options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(2);
+        }
+
+        private void SetupServices(IServiceProvider serviceProvider)
+        {
+            openAIClient ??= serviceProvider.GetRequiredService<OpenAIClient>();
+            authenticationFilter ??= serviceProvider.GetRequiredService<IAuthenticationFilter>();
         }
 
         private static async Task HealthEndpoint(HttpContext context)
@@ -133,75 +109,6 @@ namespace OpenAI.Proxy
             context.Response.ContentType = contentType;
             const string content = "OK";
             await context.Response.WriteAsync(content);
-        }
-
-        /// <summary>
-        /// Handles incoming requests, validates authentication, and forwards the request to OpenAI API
-        /// </summary>
-        private async Task HandleRequest(HttpContext httpContext, string endpoint)
-        {
-            try
-            {
-                // ReSharper disable once MethodHasAsyncOverload
-                // just in case either method is implemented we call it twice.
-                authenticationFilter.ValidateAuthentication(httpContext.Request.Headers);
-                await authenticationFilter.ValidateAuthenticationAsync(httpContext.Request.Headers);
-
-                var method = new HttpMethod(httpContext.Request.Method);
-                var uri = new Uri(string.Format(openAIClient.OpenAIClientSettings.BaseRequestUrlFormat, $"{endpoint}{httpContext.Request.QueryString}"));
-                using var openAIRequest = new HttpRequestMessage(method, uri);
-                openAIRequest.Content = new StreamContent(httpContext.Request.Body);
-
-                if (httpContext.Request.ContentType != null)
-                {
-                    openAIRequest.Content.Headers.ContentType = MediaTypeHeaderValue.Parse(httpContext.Request.ContentType);
-                }
-
-                var proxyResponse = await openAIClient.Client.SendAsync(openAIRequest, HttpCompletionOption.ResponseHeadersRead);
-                httpContext.Response.StatusCode = (int)proxyResponse.StatusCode;
-
-                foreach (var (key, value) in proxyResponse.Headers)
-                {
-                    if (ExcludedHeaders.Contains(key)) { continue; }
-                    httpContext.Response.Headers[key] = value.ToArray();
-                }
-
-                foreach (var (key, value) in proxyResponse.Content.Headers)
-                {
-                    if (ExcludedHeaders.Contains(key)) { continue; }
-                    httpContext.Response.Headers[key] = value.ToArray();
-                }
-
-                httpContext.Response.ContentType = proxyResponse.Content.Headers.ContentType?.ToString() ?? string.Empty;
-                const string streamingContent = "text/event-stream";
-
-                if (httpContext.Response.ContentType.Equals(streamingContent))
-                {
-                    var stream = await proxyResponse.Content.ReadAsStreamAsync();
-                    await WriteServerStreamEventsAsync(httpContext, stream);
-                }
-                else
-                {
-                    await proxyResponse.Content.CopyToAsync(httpContext.Response.Body);
-                }
-            }
-            catch (AuthenticationException authenticationException)
-            {
-                httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                await httpContext.Response.WriteAsync(authenticationException.Message);
-            }
-            catch (Exception e)
-            {
-                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                await httpContext.Response.WriteAsync(JsonSerializer.Serialize(new { error = new { e.Message, e.StackTrace } }));
-            }
-        }
-
-        private static async Task WriteServerStreamEventsAsync(HttpContext httpContext, Stream contentStream)
-        {
-            var responseStream = httpContext.Response.Body;
-            await contentStream.CopyToAsync(responseStream, httpContext.RequestAborted);
-            await responseStream.FlushAsync(httpContext.RequestAborted);
         }
     }
 }
