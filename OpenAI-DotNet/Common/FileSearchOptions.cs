b2c0aa75c5c523c24e8b8151d67a33d41b21b043
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-09-14 17:31:13 -0400

    OpenAI-DotNet 8.2.5 (#367)
    - Fixed ResponseObjectFormat deserialization when maxNumberOfResults is null

--- OpenAI-DotNet/Common/FileSearchOptions.cs
+++ OpenAI-DotNet/Common/FileSearchOptions.cs
@@ -9,10 +9,11 @@ namespace OpenAI
     {
         public FileSearchOptions() { }
 
-        public FileSearchOptions(int maxNumberOfResults, RankingOptions rankingOptions = null)
+        public FileSearchOptions(int? maxNumberOfResults, RankingOptions rankingOptions = null)
         {
             MaxNumberOfResults = maxNumberOfResults switch
             {
+                null => null,
                 < 1 => throw new ArgumentOutOfRangeException(nameof(maxNumberOfResults), "Max number of results must be greater than 0."),
                 > 50 => throw new ArgumentOutOfRangeException(nameof(maxNumberOfResults), "Max number of results must be less than 50."),
                 _ => maxNumberOfResults
@@ -22,10 +23,12 @@ namespace OpenAI
 
         [JsonInclude]
         [JsonPropertyName("max_num_results")]
-        public int MaxNumberOfResults { get; private set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public int? MaxNumberOfResults { get; private set; }
 
         [JsonInclude]
         [JsonPropertyName("ranking_options")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public RankingOptions RankingOptions { get; private set; }
     }
 }
