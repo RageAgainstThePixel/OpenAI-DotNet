825ee851d1da8ab3ab2f2bbcd8cf079e29b90788
StephenHodgson <rage.against.the.pixel@gmail.com>
2025-05-31 00:39:12 -0400

    ResponseEndpoint stubs

---
+++ OpenAI-DotNet/Responses/IResponseItem.cs
@@ -0,0 +1,8 @@
+﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace OpenAI.Responses
+{
+    public interface IResponseItem : IListItem
+    {
+    }
+}
