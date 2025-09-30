5ca8e3be9a47408efc668fd4ed806c4598e23d67
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-09-29 20:03:50 -0400

    - Added NoiseReductionSettings for RealtimeConfiguration

---
+++ OpenAI-DotNet/Realtime/NoiseReductionSettings.cs
@@ -0,0 +1,14 @@
+﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Text.Json.Serialization;
+
+namespace OpenAI.Realtime
+{
+    public sealed class NoiseReductionSettings
+    {
+        [JsonInclude]
+        [JsonPropertyName("type")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+        public NoiseReduction Type { get; private set; }
+    }
+}
